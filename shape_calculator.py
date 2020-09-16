d78413f7af9dc03fe5d611b564f15091da135229
Debojeet Das <debojeetofficial@gmail.com>
2020-09-16 04:31:18 +0000

    added rectangle class

--- shape_calculator.py
+++ shape_calculator.py
@@ -1,4 +1,28 @@
 class Rectangle:
+  
+  #Initialization constructor
+  def __init__(self, w, h):
+    self.width = w
+    self.height = h
+
+  #Set width method
+  def set_width(self, w):
+    self.width = w
+
+  #Set height method
+  def set_height(self, h):
+    self.height = h
+
+  #Get Area method
+  def get_area(self):
+    return (self.width * self.height)
+
+  #Get Perimeter method
+  def get_perimeter(self):
+    return ((2 * self.width) + (2 * self.height))
+
+  
+  
 
 
 
